ece5c333de319690df5a9671586baaee0c727e5d
Rudra Gowda H <121507406+Rudragowdah@users.noreply.github.com>
2024-09-28 18:23:28 +0530

    Minutes Read Value Updated

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -81,7 +81,7 @@ export default function TextForm(props) {
         <div className="container">
             <h2>Your Text Summary</h2>
             <p>{wordCount(text)} words and {text.length} characters</p>
-            <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read.</p>
+            <p>{0.008 * text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes read.</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:'Nothing to Preview'}</p>
         </div>
